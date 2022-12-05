b9c1d5342138ee5cc2a377fd813be76c1c63bec6
leobardo sanabria <leovoley2@gmail.com>
2022-12-05 18:41:46 -0500

    nueva web

--- src/data/galeria.js
+++
@@ -1,9 +0,0 @@
- const imagenes = document.querySelectorAll('img-galeria')
-//const imagenesLigth = document.querySelector('agregar-imagen')
-//const contenedorLigth = document.querySelector('imagen-ligth')
-
-imagenes.forEach(imagen => {
-    imagen.addEventListener('click', () => {
-        alert('me dolio')
-    })
-})
